7b7a51ce8281ee269def819a9a696ca97331e0b7
DnsReach <80495650+DnsReach@users.noreply.github.com>
2022-01-12 17:14:03 -0300

    Update Messages.js

--- src/message/Messages.js
+++ src/message/Messages.js
@@ -24,6 +24,7 @@ const Message = () => {
                     style={{
                         width: '40px',
                     }}
+                    alt="Avatar"
                 />
             </>
         )
